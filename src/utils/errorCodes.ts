146175db1c4d60de55b2a53bb263983f8fcfe392
Shanaka <madushanka991@gmail.com>
2024-12-29 09:09:25 +0530

    changes

---
+++ src/utils/errorCodes.ts
@@ -0,0 +1,14 @@
+export const ERROR_CODES = {
+    EMPTY_BODY: 'ER00400',
+    SERVER_ERROR: 'ER00500',
+};
+
+export const ERROR_MESSAGES = {
+    EMPTY_BODY: 'Message body should not be empty',
+    SERVER_ERROR: 'Internal error. Please contact support person',
+};
+
+export const HTTP_STATUSES = {
+    BAD_REQUEST: 'BAD_REQUEST',
+    SERVER_ERROR: 'SERVER_ERROR',
+};
